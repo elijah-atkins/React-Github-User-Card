d12550256d3a11f2d79507bf8dcce8a0cdce6a53
Elijah Atkins <elijahorama@gmail.com>
2020-05-12 19:35:31 -0600

    finished styling added github branding

---
+++ precache-manifest.fe67c817f0f9ac5c21ae883c065207a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0694e86a51567ea72e45d712c32f8ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1425f85ba4670d48a387",
+    "url": "/static/css/main.4c145fc7.chunk.css"
+  },
+  {
+    "revision": "c53b758c439e5e5a872f",
+    "url": "/static/js/2.bce32e9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bce32e9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1425f85ba4670d48a387",
+    "url": "/static/js/main.3d1a6796.chunk.js"
+  },
+  {
+    "revision": "03bafebba6984a30ca6b",
+    "url": "/static/js/runtime-main.51aeffbc.js"
+  }
+]);
